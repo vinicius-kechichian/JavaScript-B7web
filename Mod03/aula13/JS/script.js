90a364d98a20a8eaa332868b2f4a8752d5b66a7c
Vinicius Kechichian <134213761+vinicius-kechichian@users.noreply.github.com>
2023-11-16 10:05:49 -0300

    Modulo03 - aula13

---
+++ Mod03/aula13/JS/script.js
@@ -0,0 +1,17 @@
+//Elementos
+const input = document.querySelector('input');
+const lista = document.querySelector('ul');
+
+//Eventos
+input.addEventListener('keyup', handleKeyUp); /* quando soltar o enter, evento funcionara */
+
+//Funções
+function handleKeyUp(e) {
+    if(e.key === 'Enter') {
+        const newLi = document.createElement('li'); /* criar elemento na lista*/
+        newLi.innerHTML = input.value; /* pegar o valor digitado */
+        lista.appendChild(newLi); /* adicionar na lista */
+
+        input.value = ''; /* limpa campo de pesquisa */
+    }
+}
